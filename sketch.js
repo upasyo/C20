0eb5cc52c460521a0f8adaf8e5c9c5ccb61f3a77
Upasyo Kushari <69452530+upasyo@users.noreply.github.com>
2021-04-03 20:36:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
+var garden;
 var cat ,cat_steady__Img,cat_running_Img,cat_collided_Img;
-var garden,garden_Img;
 var mouse,mouse_steady_Img,mouse_enjoying_Img,mouse_collided_Img;
 
 function preload() {
-    garden_Img=loadImage("images/garden.png");
+    garden=loadImage("images/garden.png");
     cat_steady_Img=loadAnimation("images/cat1.png");
     cat_running_Img=loadAnimation("images/cat2.png","images/cat3.png");
     cat_collided_Img=loadAnimation("images/cat4.png");
@@ -16,47 +16,44 @@ function preload() {
 function setup(){
     createCanvas(1000,650);
     //create tom and jerry sprites here
-    garden=createSprite(300,300);
-    garden.addImage(garden_Img);
-
-    cat=createSprite(650,470);
+ 
+    cat=createSprite(840, 600);
     cat.addAnimation("cat_steady",cat_steady_Img);
-    cat.addAnimation("cat_running",cat_running_Img);
-    cat.addAnimation("cat_collided", cat_collided_Img);
     cat.scale=0.1;
  
 
-    mouse=createSprite(200,470);
+    mouse=createSprite(200, 600);
     mouse.addAnimation("mouse_steady",mouse_steady_Img);
-    mouse.addAnimation("mouse_running",mouse_enjoying_Img);
-    mouse.addAnimation("mouse_collided",mouse_collided_Img);
     mouse.scale=0.1;
 }
 
 function draw() {
-    background(255);
-    edges=createEdgeSprites();
-    cat.collide(edges);
-    mouse.collide(edges);
+    background(garden);
+
     //Write condition here to evalute if tom and jerry collide
-  if(cat.x-mouse.x<(cat.width-mouse.width)/2){
-    mouse.changeAnimation("mouse_collided",mouse_collided_Img);
-    cat.changeAnimation("cat_collided", cat_collided_Img);              
-  }
-   keyPressed();
+    if(cat.x-mouse.x<(cat.width-mouse.width)/2){ 
+      mouse.addAnimation("mouse_collided",mouse_collided_Img);
+      mouse.changeAnimation("mouse_collided");
+      cat.velocityX = 0; 
+      cat.addAnimation("cat_collided", cat_collided_Img);
+      cat.x=300;
+      cat.scale=0.1;
+      mouse.scale=0.1;
+      cat.changeAnimation("cat_collided"); 
+    }
+   
     drawSprites();
 }
 
 
 function keyPressed(){
-if(keyCode ===RIGHT_ARROW){
-    mouse.changeAnimation("mouse_running",mouse_enjoying_Img);
+if(keyCode ===LEFT_ARROW){
+    mouse.addAnimation("mouse_running",mouse_enjoying_Img);
+    mouse.changeAnimation("mouse_running");
     mouse.frameDelay=25;
-}
-  //For moving and changing animation write code here
-  if(keyCode ===LEFT_ARROW){
-    cat.changeAnimation("cat_running",cat_running_Img);
-    cat.frameDelay=25;
+
+    cat.addAnimation("cat_running",cat_running_Img);
+    cat.changeAnimation("cat_running");
     cat.velocityX=-2;
 }
 
